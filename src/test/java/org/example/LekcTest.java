ba091f838a8a328ebfdac3410d8f3516df70191c
s.shlyandina <svk504@gmail.com>
2025-11-23 11:23:32 +0400

    Lekc12

--- src/test/java/org/example/LekcTest.java
+++ src/test/java/org/example/LekcTest.java
@@ -3,22 +3,39 @@ package org.example;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.example.pages.YandexPage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exist;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class LekcTest {
-    YandexPage yandexPage = new YandexPage();
-
+//public class LekcTest {
+//    YandexPage yandexPage = new YandexPage();
+//
+//    @Test
+//    void test() {
+//        Selenide.sleep(5000);
+//        yandexPage.openYandexPage()
+//                .setTextInput("погода")
+//                .click()
+//                .isDisplayed();
+//            }
+//}
+class ExceptionWithTryCatch {
     @Test
-    void test() {
-        Selenide.sleep(5000);
-        yandexPage.openYandexPage()
-                .setTextInput("погода")
-                .click()
-                .isDisplayed();
-            }
+    void exception() {
+        try {
+            System.out.println((String)null);
+            Assertions.assertTrue(false);
+        }
+        catch (NullPointerException e) {
+            System.out.println("исключение" + e.getMessage());
+        }
+        catch (AssertionError error) {
+            System.out.println("исключение обработано");
+    }
+    }
 }
